e27935400bcba8760db57f950dad54032b85df66
josef <josef.gson@gmail.com>
2015-11-30 10:26:22 +0100

    adding gaol submission

--- gaol_lib.py
+++ gaol_lib.py
@@ -34,7 +34,7 @@ def get_consistency_proof(baseurl, tree_size1, tree_size2):
         params = urllib.urlencode({"first":tree_size1,
                                    "second":tree_size2})
         result = \
-          urlopen(baseurl + "ct/v1/get-sth-consistency?" + params).read()
+          urlopen(baseurl + "gaol/v1/get-sth-consistency?" + params).read()
         return json.loads(result)["consistency"]
     # except urllib2.HTTPError, e:
     #     print "ERROR:", e.read()
@@ -44,3 +44,23 @@ def extract_original_entry(entry):
     leaf_input =  base64.decodestring(entry["leaf_input"])
     (data_blob, timestamp, issuer_key_hash) = unpack_mtl(leaf_input)
     return (data_blob, timestamp)
+
+def make_blob(data):
+	return base64.b64encode(data)
+
+def add_blob(baseurl, blob):
+    try:
+        result = urlopen(baseurl + "gaol/v1/add-blob", json.dumps({"blob" : blob})).read()
+        return json.loads(result)
+    except urllib2.HTTPError, e:
+        return "ERROR " + str(e.code) + " : " + e.read()
+        # if e.code == 400:
+        return None
+        # sys.exit(1)
+    except ValueError, e:
+        print "==== FAILED REQUEST ===="
+        print submission
+        print "======= RESPONSE ======="
+        print result
+        print "========================"
+        raise e
--- gaol_test.py
+++ gaol_test.py
@@ -10,6 +10,19 @@ def test_get_entry(url, idx):
     entry = get_entries(url,idx,idx)["entries"][0]
     print "Received: " + extract_original_entry(entry)[0]
 
+def test_submission(url, data):
+    print "\nTesting to submitt a sample text..."
+    blob = make_blob(data)
+    res = add_blob(url,blob)
+    print res
+
+def test_consistency_proof(url, idx1, idx2):
+    print "\nTesing a consistency proof"
+    res = get_consistency_proof(url, idx1, idx2)
+    print res
+
 if __name__ == '__main__':
     url = CTLOGS[0]["url"]
-    test_get_entry(url,1)
+    test_get_entry(url,2)
+    # test_submission(url, "In a time of universal deceit - telling the truth is a revolutionary act.  -- George Orwell")
+    test_consistency_proof(url, 2, 3)
